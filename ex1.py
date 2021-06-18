246a846f2bfadedaf8bd6c7c74453df8fc3ec866
Julia Sousa <sousasjulia@gmail.com>
2021-06-18 08:48:37 -0300

    primeiros exercícios

---
+++ ex1.py
@@ -0,0 +1,2 @@
+msg = 'Olá Mundo, esse é o meu primeiro exercício de Python. Até onde irei chegar?'
+print(msg)
---
+++ ex2.py
@@ -0,0 +1,3 @@
+nome = input('Olá, qual é o seu nome?')
+msg = 'É um prazer te conhecer, {}!'.format(nome)
+print(msg)
